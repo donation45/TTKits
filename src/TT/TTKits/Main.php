b3323c82bce71d5098eaf537c872c3f873a8db30
Alianated <seanmcpex@gmail.com>
2015-10-12 08:19:17 -0400

    Update Main.php

--- src/TT/TTKits/Main.php
+++ src/TT/TTKits/Main.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace TT\TTKits;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase implements Listener{
+
+     public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info(TextFormat::GREEN . "TTKits enabled!");
+     }
+     public function onDisable(){
+        $this->getLogger()->info(TextFormat::RED . "TTKits disabled!");
+     }
+    public function onCommand(CommandSender $sender, Command $cmd, $label,array $args){
+        switch(strtolower($cmd->getName())){
+        case "kit":
+                 if($sender instanceof Player and $args[1] == "assassin"){
+                    $sender->sendMessage(TextFormat::GREEN . "You have chosen the Assassin kit.");
+                 }
+                 else
+                 {
+                       $sender->sendMessage($cmd->getUsage);
+                 }   
+        return true;
+        }
+    }
+}
