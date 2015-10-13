f32dfdb892cce46d33cc81c74145d3b199c8d8bb
Angelo <Angelo@Angelo-PC.attlocal.net>
2015-10-13 04:24:06 -0700

    API

--- src/TT/TTKits/Main.php
+++ src/TT/TTKits/Main.php
@@ -11,16 +11,21 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\entity\Effect;
 use pocketmine\item\Item;
+// EVENTS
+use pocketmine\event\player\PlayerQuitEvent;
 
 class Main extends PluginBase implements Listener{
+    
+    private $players = array();
 
-     public function onEnable(){
+    public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GREEN . "TTKits enabled!");
      }
      public function onDisable(){
         $this->getLogger()->info(TextFormat::RED . "TTKits disabled!");
      }
+
      public function onCommand(CommandSender $sender, Command $cmd, $label,array $args){
         switch(strtolower($cmd->getName())){
         case "kit":
@@ -28,43 +33,13 @@ class Main extends PluginBase implements Listener{
                     $kit = strtolower($args[0]);
                     switch($kit) {
                     case "assassin":
-                        $sender->sendMessage($this->colorize("&aYou have chosen the &cAssassin&a kit."));
-                        $sender->getInventory()->clearAll();
-                        $sender->getInventory()->setHelmet(Item::get(298));
-                        $sender->getInventory()->setChestplate(Item::get(299));
-                        $sender->getInventory()->setLeggings(Item::get(316));
-                        $sender->getInventory()->setBoots(Item::get(301));
-                        $sender->getInventory()->sendArmorContents($sender);
-                        $sword = Item::fromString("iron_sword");
-                        $sword->setCount(1);
-                        $sender->getInventory()->addItem($sword);
-                        // Will need to set identifier soon.
+                        $this->setKit($sender, 1);
                         break;
                     case "tank":
-                        $sender->sendMessage($this->colorize("&aYou have chosen the &9Tank&a kit."));
-                        $sender->getInventory()->clearAll();
-                        $sender->getInventory()->setHelmet(Item::get(306));
-                        $sender->getInventory()->setChestplate(Item::get(303));
-                        $sender->getInventory()->setLeggings(Item::get(304));
-                        $sender->getInventory()->setBoots(Item::get(309));
-                        $sender->getInventory()->sendArmorContents($sender);
-                        $sword = Item::fromString("wooden_sword");
-                        $sword->setCount(1);
-                        $sender->getInventory()->addItem($sword);
-                        // Will need to set identifier soon.
+                        $this->setKit($sender, 2);
                         break;
                     case "spectre":
-                        $sender->sendMessage($this->colorize("&aYou have chosen the &8Spectre&a kit."));
-                        $sender->getInventory()->clearAll();
-                        $sender->getInventory()->setBoots(Item::get(305));
-                        $sender->getInventory()->sendArmorContents($sender);
-                        $axe = Item::fromString("stone_axe");
-                        $axe->setCount(1);
-                        $sender->getInventory()->addItem($axe);
-                        $sugar = Item::fromString("sugar");
-                        $sugar->setCount(1);
-                        $sender->getInventory()->addItem($sugar);
-                        // Will need to set identifier soon.
+                        $this->setKit($sender, 3);
                         break;
                     default:
                         $sender->sendMessage($this->colorize($cmd->getUsage));
@@ -80,16 +55,109 @@ class Main extends PluginBase implements Listener{
         }
     }
     
+    /*
+         ___      .______    __  
+        /   \     |   _  \  |  | 
+       /  ^  \    |  |_)  | |  | 
+      /  /_\  \   |   ___/  |  | 
+     /  _____  \  |  |      |  | 
+    /__/     \__\ | _|      |__| 
+     */
+    
+    /**
+     * Adds a player from kit mode. Ex: Player runs /kit
+     * 
+     * @param Player $player Player that will be removed kit mode.
+     * @param int $id Kit ID, see cheatsheat!
+     * @return boolean If false, player is not in kit mode.
+     */
+    public function setKit(Player $player, $id = 0) {
+        $player_name = $player->getName();
+        $player->getInventory()->clearAll();
+        switch($id) {
+            case 0:
+                $player->sendMessage($this->colorize("&cKit Removed!"));
+                $this->players[$player_name] = 0; //Will be unset on death or /leave maybe?
+                break;
+            case 1:
+                $player->sendMessage($this->colorize("&aYou have chosen the &cAssassin&a kit."));
+                $player->getInventory()->setHelmet(Item::get(298));
+                $player->getInventory()->setChestplate(Item::get(299));
+                $player->getInventory()->setLeggings(Item::get(316));
+                $player->getInventory()->setBoots(Item::get(301));
+                $player->getInventory()->sendArmorContents($player);
+                $sword = Item::fromString("iron_sword");
+                $sword->setCount(1);
+                $player->getInventory()->addItem($sword);
+                $this->players[$player_name] = 1; //Will be unset on death or /leave maybe?
+                break;
+            case 2:
+                $player->sendMessage($this->colorize("&aYou have chosen the &9Tank&a kit."));
+                $player->getInventory()->setHelmet(Item::get(306));
+                $player->getInventory()->setChestplate(Item::get(303));
+                $player->getInventory()->setLeggings(Item::get(304));
+                $player->getInventory()->setBoots(Item::get(309));
+                $player->getInventory()->sendArmorContents($player);
+                $sword = Item::fromString("wooden_sword");
+                $sword->setCount(1);
+                $player->getInventory()->addItem($sword);
+                $this->players[$player_name] = 2; //Will be unset on death or /leave maybe?
+                break;
+            case 3:
+                $player->sendMessage($this->colorize("&aYou have chosen the &8Spectre&a kit."));
+                $player->getInventory()->setBoots(Item::get(305));
+                $player->getInventory()->sendArmorContents($sender);
+                $axe = Item::fromString("stone_axe");
+                $axe->setCount(1);
+                $player->getInventory()->addItem($axe);
+                $sugar = Item::fromString("sugar");
+                $sugar->setCount(1);
+                $player->getInventory()->addItem($sugar);
+                $this->players[$player_name] = 3; //Will be unset on death or /leave maybe?
+                break;
+        }
+    }
+    
+    
+    /**
+     * Removes a player from kit mode. Ex: Player leaves
+     * 
+     * @param Player $player Player that will be removed kit mode.
+     * @return boolean If false, player is not in kit mode.
+     */
+    public function removePlayer(Player $player) {
+        $player_name = $player->getName();
+        if(isset($this->players[$player_name])) {
+            unset($this->players[$player_name]);
+            $player->getInventory()->clearAll();
+            return;
+        } else {
+            return false;
+        }
+    }
     /**
      * Will be used to color our messages.
      * This makes it easier and allows us to make the messages beautiful!
      * 
      * @param string $str The string must contain '&' for this function to do anything!
-     * 
      * @return string Returns the string with '§' instead of '&'.
      */
     public function colorize($str) {
         $new_str = str_replace("&", "§", $str);
         return $new_str;
+    }
+    
+    /* 
+     ___________    ____  _______ .__   __. .___________.    _______.
+    |   ____\   \  /   / |   ____||  \ |  | |           |   /       |
+    |  |__   \   \/   /  |  |__   |   \|  | `---|  |----`  |   (----`
+    |   __|   \      /   |   __|  |  . `  |     |  |        \   \    
+    |  |____   \    /    |  |____ |  |\   |     |  |    .----)   |   
+    |_______|   \__/     |_______||__| \__|     |__|    |_______/   
+     */
+    
+    public function onQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $this->removePlayer($player);
     }
 }
